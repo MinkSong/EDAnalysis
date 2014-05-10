5e7a685af7c3589ce53d3579b8f9ae85343cc530
MinkSong <randolfson@me.com>
2014-05-10 16:39:11 +0900

    Plot1

--- plot1.R
+++ plot1.R
@@ -1,11 +1,14 @@
+#Read datasets from textfile
 data=read.table("household_power_consumption.txt",header=TRUE,sep=";", na.strings= "?")
 
+#subset datasets from origin.
 plot1<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
 
 
+#let's do some graphic work
 
 png("plot1.png", width = 480, height = 480)
 
-hist(sub$Global_active_power, type="l", main="Global Active Power",col="red",ylab= "Frequency", xlab="Global Active Power (kilowatts)")
+hist(plot1$Global_active_power, main="Global Active Power",col="red",ylab= "Frequency", xlab="Global Active Power(kilowatts)")
 
 dev.off()
